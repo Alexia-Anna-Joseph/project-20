fc1217033ae365b5778252cb4fae9c17bb40aa79
Alexia-Anna-Joseph <78249362+Alexia-Anna-Joseph@users.noreply.github.com>
2021-03-18 11:17:03 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,50 +1,57 @@
-var wall;
-var bullet,speed,weight;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var dustbin;
+var ball;
+var ground;
+
+function preload()
+{
+	
+}
 
 function setup() {
-  createCanvas(1500,657);
+	createCanvas(1300, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	ball = new PAPER(100,200,20);
+    ground = new GROUND(750,610,1500,100)
+    side1 = new DUSTBIN (930,485,20,120)
+    side2 = new DUSTBIN (1140, 485, 20, 120)
+    side3 = new DUSTBIN (1036, 550, 230, 20)
+
+	Engine.run(engine);
   
-  thickness=random(22,83)
-  speed=random(223,321)
-  weight=random(30,52)
-
-  bullet=createSprite(50,200,50,5);
-  bullet.velocityX = speed;
-  bullet.shapeColor=color(255);
-
-  wall=createSprite(1200,200,thickness,height/2)
-  wall.shapeColor=color(80,80,80)
 }
 
-function hasCollided(bullet,wall) {
-  bulletRightEdge=bullet.x + bullet.width ; 
-  wallLeftEdge = wall.x;
-  if(bulletRightEdge>=wallLeftEdge) {
-    return true ; 
-  }
-return false ; 
-}
 
 function draw() {
-  background(0);  
+  rectMode(CENTER);
+  background(0);
 
-  bullet.velocityX = speed ;  
-if(hasCollided(bullet,wall)) {
+  Engine.update(engine);
 
-
- 
-  var damage = 0.5 * weight * speed * speed / (thickness * thickness * thickness); 
-
-if(damage>10) {
-  wall.shapeColor = color(255,0,0);
-}
-if(damage<10) { 
-  wall.shapeColor = color(0,255,0);
-}
-
-}
-
-
-     
+  ground.display();
+  side1.display();
+  side2.display();
+  side3.display();
+  ball.display();
+  
   drawSprites();
+ 
 }
+
+function keyPressed(){
+    if(keyCode===UP_ARROW){
+        Matter.Body.applyForce(ball.body,ball.body.position,{x:85,y:-85});
+    }
+}
+
+
+
